970ef185444b5b9c5fd4db5d9de69a0bfb0be76f
Jose Mamani <JOSEMLI@example.com>
2020-02-11 18:52:05 -0500

    enviar un mensaje y nos devuelve el mensaje en firebase

--- app/src/main/java/com/example/android_u2_tema4_holafirebase/MainActivity.java
+++ app/src/main/java/com/example/android_u2_tema4_holafirebase/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android_u2_tema4_holafirebase;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,25 +15,49 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity  extends AppCompatActivity {
-  private static final String PATH_START="start";
-  private static final String PATH_MESSAGE="message";
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+  FirebaseDatabase database;
+  DatabaseReference reference ;
+
+  private static final String PATH_START = "start";
+  private static final String PATH_MESSAGE = "message";
+  @BindView(R.id.tvMessage)
+  TextView tvMessage;
+  @BindView(R.id.edtmessage)
+  EditText edtmessage;
+  @BindView(R.id.btnmessage)
+  Button btnmessage;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    final TextView tvMessage=findViewById(R.id.tvMessage);
-    FirebaseDatabase database= FirebaseDatabase.getInstance();
-    final DatabaseReference reference=database.getReference(PATH_START).child(PATH_MESSAGE);
+    ButterKnife.bind(this);
+    final TextView tvMessage = findViewById(R.id.tvMessage);
+
+    //ponemos como global estas dos lineas
+    database = FirebaseDatabase.getInstance();
+    reference = database.getReference(PATH_START).child(PATH_MESSAGE);
+
     reference.addValueEventListener(new ValueEventListener() {
       @Override
       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
         tvMessage.setText(dataSnapshot.getValue(String.class));
       }
+
       @Override
       public void onCancelled(@NonNull DatabaseError databaseError) {
-        Toast.makeText(MainActivity.this,"Error al consultar en Firebase", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Error al consultar en Firebase", Toast.LENGTH_LONG).show();
       }
     });
+  }
+
+  @OnClick(R.id.btnmessage)
+  public void onViewClicked() {
+    reference.setValue(edtmessage.getText().toString().trim());
   }
 }
